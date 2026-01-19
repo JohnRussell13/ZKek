9f06c7ad5612fea6c19519d29f2bc59a3f61efd7
JohnRussell13 <aleksandar.g1999@gmail.com>
2026-01-20 00:32:33 +0100

    Updated scripts

--- circom/scripts/compile.sh
+++ circom/scripts/compile.sh
@@ -1,12 +1,23 @@
-circom zkek.circom --r1cs --wasm
+#!/usr/bin/env bash
+set -e
+
+mkdir -p build/r1cs
+cd build/r1cs
+
+circom ../../circuits/zkek.circom --r1cs --wasm
+
+cd ..
+mkdir -p zkey
+cd zkey
 
 snarkjs ptn bn128 12 pot12_0000.ptau
 echo "input_value" | snarkjs ptc pot12_0000.ptau pot12_0001.ptau
 
 snarkjs pt2 pot12_0001.ptau pot12_final.ptau
 
-snarkjs g16s zkek.r1cs pot12_final.ptau zkek_0000.zkey
+snarkjs g16s ../r1cs/zkek.r1cs pot12_final.ptau zkek_0000.zkey
 echo "input_value" | snarkjs zkc zkek_0000.zkey zkek_0001.zkey
+
 snarkjs zkev zkek_0001.zkey verification_key.json
 
 rm pot12_0000.ptau pot12_0001.ptau pot12_final.ptau
--- circom/scripts/prove.sh
+++ circom/scripts/prove.sh
@@ -1,1 +1,7 @@
-snarkjs g16f inputs.json zkek_js/zkek.wasm zkek_0001.zkey proof.json public.json
+#!/usr/bin/env bash
+set -e
+
+mkdir -p build/proof
+cd build/proof
+
+snarkjs g16f ../../inputs/inputs.json ../r1cs/zkek_js/zkek.wasm ../zkey/zkek_0001.zkey proof.json public.json
